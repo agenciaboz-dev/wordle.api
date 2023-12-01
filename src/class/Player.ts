36a754becab9879e5c51441c320491dba5bca01a
nandobfer <nandobfer@yahoo.com.br>
2023-11-30 23:00:53 -0300

    feat: player avatar

--- src/class/Player.ts
+++ src/class/Player.ts
@@ -4,19 +4,20 @@ import { uid } from "uid"
 export class Player {
     id: string
     name: string
+    avatar: string
     score: number = 0
     history: string[] = []
 
     socket: Socket
 
-    constructor(data: NewPlayer, socket:Socket) {
+    constructor(data: NewPlayer, socket: Socket) {
         this.id = uid()
         this.name = data.name
+        this.avatar = data.avatar
         this.socket = socket
     }
 
     toJSON() {
-        return {...this, socket: null}
+        return { ...this, socket: null }
     }
-
 }
